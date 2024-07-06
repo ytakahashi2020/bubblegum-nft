06f21e5565586b18f57d181560520fe560447f44
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-07-06 15:40:59 +0900

    add create nft

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -7,7 +7,12 @@ import { createTree } from "@metaplex-foundation/mpl-bubblegum";
 import {
   fetchMerkleTree,
   fetchTreeConfigFromSeeds,
+  mintV1,
+  findLeafAssetIdPda,
+  parseLeafFromMintV1Transaction,
+  LeafSchema,
 } from "@metaplex-foundation/mpl-bubblegum";
+import { none } from "@metaplex-foundation/umi";
 
 export const createBubbleGumTrees = async (wallet: any) => {
   const endpoint = "https://api.devnet.solana.com";
@@ -15,6 +20,7 @@ export const createBubbleGumTrees = async (wallet: any) => {
 
   umi.use(walletAdapterIdentity(wallet));
 
+  // ツリーを作成　ここから
   const merkleTree = generateSigner(umi);
 
   const builder = await createTree(umi, {
@@ -25,7 +31,9 @@ export const createBubbleGumTrees = async (wallet: any) => {
 
   await builder.sendAndConfirm(umi);
   console.log("asset =>", merkleTree.publicKey.toString());
+  // ツリーを作成　ここから
 
+  // ツリーを取得 ここから
   const merkleTreeAccount = await fetchMerkleTree(umi, merkleTree.publicKey);
   console.log("merkleTreeAccount =>", merkleTreeAccount);
 
@@ -33,6 +41,33 @@ export const createBubbleGumTrees = async (wallet: any) => {
     merkleTree: merkleTree.publicKey,
   });
   console.log("treeConfig =>", treeConfig);
+  // ツリーを取得 ここまで
+
+  // NFTを作成 ここから
+  const { signature } = await mintV1(umi, {
+    leafOwner: wallet.publicKey,
+    merkleTree: merkleTree.publicKey,
+    metadata: {
+      name: "My Compressed NFT",
+      uri: "https://example.com/my-cnft.json",
+      sellerFeeBasisPoints: 500, // 5%
+      collection: none(),
+      creators: [
+        { address: umi.identity.publicKey, verified: false, share: 100 },
+      ],
+    },
+  }).sendAndConfirm(umi);
+  // NFTを作成 ここまで
+
+  // NFTを取得 ここから
+  const leaf: LeafSchema = await parseLeafFromMintV1Transaction(umi, signature);
+  const assetId = findLeafAssetIdPda(umi, {
+    merkleTree: merkleTree.publicKey,
+    leafIndex: leaf.nonce,
+  });
+
+  console.log("assetId =>", assetId);
+  // NFTを取得 ここまで
 
   return merkleTree.publicKey.toString();
 };
